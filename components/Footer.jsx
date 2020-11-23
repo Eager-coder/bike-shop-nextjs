9d5d0f86c4732cdad675d0da9903d190defa64de
Turan Sultan <turan.sultann@gmail.com>
2020-11-23 22:24:00 +0600

    another style changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -21,12 +21,6 @@ const StyledFooter = styled.footer`
 						filter: invert(1);
 					}
 				}
-				.payment {
-					img {
-						margin: 5px 10px 5px 0;
-						width: 40px;
-					}
-				}
 			}
 			.links {
 				display: flex;
@@ -53,108 +47,109 @@ const StyledFooter = styled.footer`
 					}
 				}
 			}
-			.social-media {
-				display: flex;
-				a {
-					display: flex;
-					justify-content: center;
-					align-items: center;
-					border-radius: 50%;
-					padding: 10px;
-					width: 54px;
-					height: 54px;
-					background-color: rgb(44, 44, 44);
-					margin-left: 20px;
-					:hover > img {
-						filter: invert(31%) sepia(51%) saturate(2291%) hue-rotate(340deg)
-							brightness(115%) contrast(101%);
-					}
-					img {
-						width: 100%;
-						height: auto;
-						filter: invert(1);
-						transition: 0.2s;
-					}
-				}
-			}
-			.bottom-items {
-				display: flex;
-				justify-content: space-between;
-				align-items: center;
-				.bottom-links {
-					display: flex;
-					justify-content: space-between;
-					align-items: center;
-					li {
-						margin-right: 20px;
-						a {
-							font-size: 0.8rem;
-							color: gray;
-						}
-					}
-					.date > span {
-						letter-spacing: 0.5px;
-						font-weight: normal;
-						font-size: 0.8rem;
-						color: #8a9496;
-					}
-				}
-			}
 		}
-	}
-
-	@media screen and (max-width: 1000px) {
-		.container {
-			/* padding: 50px 100px; */
-		}
-		.flex-container {
-			display: block;
-			margin-bottom: 50px;
-		}
-		.links {
-			width: 100%;
-			margin: 30px 0 100px 0;
-			font-size: 1rem;
-		}
-		.social-media {
-			margin-left: 0;
-		}
-		.social-media > div {
-			margin: 0;
-			margin-right: 20px;
-		}
-		.bottom-links {
+		.bottom-items {
 			display: flex;
 			justify-content: space-between;
 			align-items: center;
-			flex-wrap: wrap;
-			width: 60%;
-			height: 5rem;
+			.bottom-links {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				li {
+					margin-right: 20px;
+					a {
+						font-size: 0.8rem;
+						color: gray;
+					}
+				}
+				.date > span {
+					letter-spacing: 0.5px;
+					font-weight: normal;
+					font-size: 0.8rem;
+					color: #8a9496;
+				}
+			}
 		}
-		.date > span,
-		li > a {
-			font-size: 1rem;
+		@media screen and (max-width: 1000px) {
+			.flex-container {
+				display: block;
+				margin-bottom: 50px;
+			}
+			.links {
+				width: 100%;
+				margin: 30px 0 100px 0;
+				font-size: 1rem;
+			}
+			.social-media {
+				margin-left: 0;
+				div {
+					margin: 0;
+					margin-right: 20px;
+				}
+			}
+
+			.bottom-links {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				flex-wrap: wrap;
+				width: 60%;
+				height: 5rem;
+			}
+			.date > span,
+			li > a {
+				font-size: 1rem;
+			}
+		}
+		@media screen and (max-width: 770px) {
+			padding: 20px 15px;
+			.links {
+				display: block;
+			}
+			.link-column {
+				margin: 50px 0;
+			}
+			.bottom-links {
+				flex-wrap: nowrap;
+				width: unset;
+				height: unset;
+			}
+			.date > span,
+			li > a {
+				font-size: 0.7rem;
+			}
 		}
 	}
-	@media screen and (max-width: 770px) {
-		.container {
-			padding: 20px 15px;
+`
+const SocialMedia = styled.div`
+	display: flex;
+	a {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 50%;
+		padding: 10px;
+		width: 54px;
+		height: 54px;
+		background-color: rgb(44, 44, 44);
+		margin-left: 20px;
+		:hover > img {
+			filter: invert(31%) sepia(51%) saturate(2291%) hue-rotate(340deg)
+				brightness(115%) contrast(101%);
 		}
-		.links {
-			display: block;
+		img {
+			width: 100%;
+			height: auto;
+			filter: invert(1);
+			transition: 0.2s;
 		}
-		.link-column {
-			margin: 50px 0;
-		}
-		.bottom-links {
-			flex-wrap: nowrap;
-			width: unset;
-			height: unset;
-		}
-		.date > span,
-		li > a {
-			font-size: 0.7rem;
-		}
+	}
+`
+const PaymentCards = styled.div`
+	img {
+		margin: 5px 10px 5px 0;
+		width: 40px;
 	}
 `
 export default function Footer() {
@@ -171,7 +166,7 @@ export default function Footer() {
 								/>
 							</a>
 						</Link>
-						<div className="payment">
+						<PaymentCards>
 							<img
 								src="https://focus-bike-shop.vercel.app/icons/visa.png"
 								alt=""
@@ -197,7 +192,7 @@ export default function Footer() {
 								src="https://focus-bike-shop.vercel.app/icons/jcb.png"
 								alt=""
 							/>
-						</div>
+						</PaymentCards>
 					</div>
 
 					<div className="links">
@@ -232,7 +227,7 @@ export default function Footer() {
 							</a>
 						</div>
 					</div>
-					<div className="social-media">
+					<SocialMedia>
 						<a href="https://instagram.com" target="_blank">
 							<img
 								src="https://focus-bike-shop.vercel.app/icons/014-instagram.svg"
@@ -251,8 +246,7 @@ export default function Footer() {
 								alt=""
 							/>
 						</a>
-						<div></div>
-					</div>
+					</SocialMedia>
 				</div>
 
 				<div className="bottom-items">
